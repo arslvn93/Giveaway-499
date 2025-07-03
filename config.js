64f8b367c650b3745b3a559c088b1f69d7157efb
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-03 13:40:50 -0400

    Update Site

--- config.js
+++ config.js
@@ -145,7 +145,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1751564425106_0",
+      "id": "q_auto_1751564449871_0",
       "questionText": "What type of activities do you and your family typically enjoy during a weekend?",
       "options": [
         {
@@ -167,7 +167,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751564425106_1",
+      "id": "q_auto_1751564449871_1",
       "questionText": "When considering features in a new home, which of the following is most important to you?",
       "options": [
         {
@@ -189,7 +189,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751564425106_2",
+      "id": "q_auto_1751564449871_2",
       "questionText": "How would you describe your current living situation?",
       "options": [
         {
@@ -211,7 +211,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751564425106_3",
+      "id": "q_auto_1751564449871_3",
       "questionText": "What do you consider an ideal environment for raising a family?",
       "options": [
         {
@@ -257,8 +257,8 @@ const config = {
       "email": "acaputorealty@gmail.com"
     },
     "footerDisclaimer": "This giveaway is not sponsored by or affiliated with Toronto FC or BMO Field. Winner will be selected randomly from all eligible entries received by July 7, 2025. Must be 18+ to enter.",
-    "organizerLogoUrl": "https://images.seeklogo.com/logo-png/32/2/remax-logo-png_seeklogo-328735.png",
-    "secondaryLogoUrl": "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/480070380_1381671406492908_2989011623697879901_n.jpg?stp=dst-jpg_tt6&cstp=mx1230x693&ctp=s1230x693&_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=oPPOZX6nGHkQ7kNvwETAAO8&_nc_oc=AdlI4JZLVOU5uQw1MLHXtkoiZaSFataBp0Gc-eVLPTZE4kkyr17X6f2OwS8wZZqJGkg&_nc_zt=23&_nc_ht=scontent-sea1-1.xx&_nc_gid=o412f13POOukzZJrZpB0CQ&oh=00_AfMl216Im8LAI7AGQO2lzabbArpYi2fe1Okqk5GsAF0kSA&oe=686CA20C",
+    "organizerLogoUrl": "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/480070380_1381671406492908_2989011623697879901_n.jpg?stp=dst-jpg_tt6&cstp=mx1230x693&ctp=s1230x693&_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=oPPOZX6nGHkQ7kNvwETAAO8&_nc_oc=AdlI4JZLVOU5uQw1MLHXtkoiZaSFataBp0Gc-eVLPTZE4kkyr17X6f2OwS8wZZqJGkg&_nc_zt=23&_nc_ht=scontent-sea1-1.xx&_nc_gid=o412f13POOukzZJrZpB0CQ&oh=00_AfMl216Im8LAI7AGQO2lzabbArpYi2fe1Okqk5GsAF0kSA&oe=686CA20C",
+    "secondaryLogoUrl": "https://images.seeklogo.com/logo-png/32/2/remax-logo-png_seeklogo-328735.png",
     "secondaryLogoAlt": "",
     "organizerName": "Anthony Caputo",
     "brokerageName": "RE/MAX House of Real Estate",
