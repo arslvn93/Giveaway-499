0365b81cbf15e0b3e937ee973362604c8b214e0b
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-04 12:05:47 -0400

    Update Site

--- config.js
+++ config.js
@@ -145,7 +145,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1751644637063_0",
+      "id": "q_auto_1751645146846_0",
       "questionText": "What type of activities do you and your family typically enjoy during a weekend?",
       "options": [
         {
@@ -167,7 +167,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751644637063_1",
+      "id": "q_auto_1751645146846_1",
       "questionText": "When considering features in a new home, which of the following is most important to you?",
       "options": [
         {
@@ -189,7 +189,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751644637063_2",
+      "id": "q_auto_1751645146846_2",
       "questionText": "How would you describe your current living situation?",
       "options": [
         {
@@ -211,7 +211,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751644637063_3",
+      "id": "q_auto_1751645146846_3",
       "questionText": "What do you consider an ideal environment for raising a family?",
       "options": [
         {
@@ -233,7 +233,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751644637063_4",
+      "id": "q_auto_1751645146846_4",
       "questionText": "Is your mortgage up for renewal in the next:",
       "options": [
         {
