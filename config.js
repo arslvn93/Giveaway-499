1008cd9475c62d7f08f9f8f92896f5bc892c4e88
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-04 17:14:09 -0400

    Update Site

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const config = {
     "showCountdownInHero": true,
     "theme": "light",
     "primaryColorOverride": "#b61619",
-    "ghlWebhookUrl": "",
+    "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/sOCXFADErl7YnAppyjkb/webhook-trigger/d1ryyGnWSyjJBuySHKaK",
     "facebookPixelId": "1057475448873422",
     "salesGeniusAppApi": "",
     "followUpBossEmail": "anthony.caputo.real.estate@followupboss.me",
@@ -149,7 +149,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1751656329989_0",
+      "id": "q_auto_1751663648482_0",
       "questionText": "What type of activities do you and your family typically enjoy during a weekend?",
       "options": [
         {
@@ -171,7 +171,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751656329989_1",
+      "id": "q_auto_1751663648482_1",
       "questionText": "When considering features in a new home, which of the following is most important to you?",
       "options": [
         {
@@ -193,7 +193,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751656329989_2",
+      "id": "q_auto_1751663648482_2",
       "questionText": "How would you describe your current living situation?",
       "options": [
         {
@@ -215,7 +215,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751656329989_3",
+      "id": "q_auto_1751663648482_3",
       "questionText": "What do you consider an ideal environment for raising a family?",
       "options": [
         {
@@ -237,7 +237,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1751656329989_4",
+      "id": "q_auto_1751663648482_4",
       "questionText": "Is your mortgage up for renewal in the next:",
       "options": [
         {
